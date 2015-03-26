25ad5e0fb7c985158433e1930c27a0a938fdffe9
Enrico Schwendig <enrico.schwendig@devbliss.com>
2015-03-26 20:33:03 +0100

    fix code style

--- tasks/contractTest.js
+++ tasks/contractTest.js
@@ -12,6 +12,61 @@ var extendGruntPlugin = require('extend-grunt-plugin');
 
 var contractTest = function (grunt) {
 
+    var registerTasks = function (options) {
+        var shellServerStart = {
+            command: 'BUNDLE_GEMFILE="node_modules/grunt-pact-js/Gemfile"  bundle exec pact-mock-service restart -p ' +
+            options.port + ' -l  ' +
+            options.pactDir + '/pact.log --pact-dir ' +
+            options.pactDir + '/pacts',
+            options: {
+                stdout: true,
+                stderr: true,
+                failOnError: true,
+                async: true
+            }
+        };
+
+        var shellServerStop = {
+            command: 'BUNDLE_GEMFILE="node_modules/grunt-pact-js/Gemfile" bundle exec pact-mock-service stop -p ' + options.port,
+            options: {
+                stdout: true,
+                stderr: true,
+                failOnError: true,
+                async: false
+            }
+        };
+
+        var wait = {
+            options: {
+                delay: 1000
+            }
+        }
+
+        var karmaPact = {
+            configFile: options.karmaConfigFile,
+            singleRun: true
+
+        };
+
+
+        extendGruntPlugin(grunt, require('grunt-shell-spawn/tasks/shell'), {
+            'shell.serverStart': shellServerStart,
+            'shell.serverStop': shellServerStop
+        });
+
+        extendGruntPlugin(grunt, require('grunt-wait/tasks/wait'), {
+            'wait.pact': wait
+        });
+
+        extendGruntPlugin(grunt, require('grunt-karma/tasks/grunt-karma'), {
+            'karma.pact': karmaPact
+        });
+
+        extendGruntPlugin(grunt, require('grunt-force-task/tasks/force'), {
+            'force.karma': 'pact'
+        });
+    }
+
     grunt.registerTask("warning-exit", "Call process.exit", function () {
         process.exit(3);
     });
@@ -25,60 +80,7 @@ var contractTest = function (grunt) {
                 pactDir: 'tmp'
             });
 
-
-            var shellServerStart = {
-                command: 'BUNDLE_GEMFILE="node_modules/grunt-pact-js/Gemfile"  bundle exec pact-mock-service restart -p ' +
-                options.port + ' -l  ' +
-                options.pactDir + '/pact.log --pact-dir ' +
-                options.pactDir + '/pacts',
-                options: {
-                    stdout: true,
-                    stderr: true,
-                    failOnError: true,
-                    async: true
-                }
-            };
-
-            var shellServerStop = {
-                command: 'BUNDLE_GEMFILE="node_modules/grunt-pact-js/Gemfile" bundle exec pact-mock-service stop -p ' + options.port,
-                options: {
-                    stdout: true,
-                    stderr: true,
-                    failOnError: true,
-                    async: false
-                }
-            };
-
-            var wait = {
-                options: {
-                    delay: 1000
-                }
-            }
-
-            var karmaPact = {
-                configFile: options.karmaConfigFile,
-                singleRun: true
-
-            };
-
-
-            extendGruntPlugin(grunt, require('grunt-shell-spawn/tasks/shell'), {
-                'shell.serverStart': shellServerStart,
-                'shell.serverStop': shellServerStop
-            });
-
-            extendGruntPlugin(grunt, require('grunt-wait/tasks/wait'), {
-                'wait.pact': wait
-            });
-
-            extendGruntPlugin(grunt, require('grunt-karma/tasks/grunt-karma'), {
-                'karma.pact': karmaPact
-            });
-
-            extendGruntPlugin(grunt, require('grunt-force-task/tasks/force'), {
-                'force.karma': 'pact'
-            });
-
+            registerTasks(options);
 
             grunt.warn = grunt.fail.warn = function (warning, callback) {
                 grunt.log.warn(warning);
